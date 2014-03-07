accea30abea5adedd5a3e63ce3a828f4545024e9
cfudge <cfudge@ualbeta.ca>
2014-03-07 16:12:36 -0700

    asfd

--- server_s.c
+++ server_s.c
@@ -51,9 +51,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 #include "server_s.h"
 
-char *str_from_file(FILE *requested_file, int *file_size);
+char *str_from_file(FILE *requested_file, int req_filed, int *file_size);
 
 /* we use this structure to keep track of each connection to us */
 struct con {
@@ -65,6 +66,9 @@ struct con {
 	char *bp;	/* where we are in the buffer */
 	size_t bs;	/* total size of the buffer */
 	size_t bl;	/* how much we have left to read/write */
+        char log_string[300];
+        int is_OK;
+        int file_size;
 };
 
 /*
@@ -84,12 +88,11 @@ struct con {
  * you may have a few more files open than just your sockets
  * in order to do anything really useful
  */
+void get_date(char *date_time);
 #define MAXCONN 256
 struct con connections[MAXCONN];
 
-#define REQ_BUFFER_SIZE 256;
-
-#define BUF_ASIZE 256 /* how much buf will we allocate at a time. */
+#define BUF_ASIZE 1000 /* how much buf will we allocate at a time. */
 
 /* states used in struct con. */
 #define STATE_UNUSED 0
@@ -98,6 +101,7 @@ struct con connections[MAXCONN];
 
 #define DEBUG 1
 
+#define MAX_DATE_STRING 200
 FILE * err_file;
 
 static void usage()
@@ -127,7 +131,7 @@ struct con * get_free_conn()
  * close or initialize a connection - resets a connection to the default,
  * unused state.
  */
-void closecon (struct con *cp, int initflag)
+void closecon (struct con *cp, int initflag, char *argv[])
 {
 	if (!initflag) {
 		if (cp->sd != -1)
@@ -147,7 +151,7 @@ void closecon (struct con *cp, int initflag)
  * by using select(). once we write everything out, change the
  * state of the connection to the reading state.
  */
-void handlewrite(struct con *cp)
+void handlewrite(struct con *cp, char *argv[])
 {
 	ssize_t i;
 
@@ -167,7 +171,7 @@ void handlewrite(struct con *cp)
 	if (i == -1) {
 		if (errno != EAGAIN) {
 			/* the write failed */
-			closecon(cp, 0);
+			closecon(cp, 0, argv);
 		}
 		/*
 		 * note if EAGAIN, we just return, and let our caller
@@ -193,12 +197,14 @@ void handlewrite(struct con *cp)
  * are reading, change the state of this connection to the writing
  * state.
  */
-void handleread(struct con *cp, char *arg_path)
+void handleread(struct con *cp, char *argv[])
 {
 	ssize_t i;
-	int file_size;
-	char *c;
+	int file_size, req_filed;
+	char c1, c2;
 	size_t file_chars;
+        char itoa[12];
+	char *complete_path;
 	char *file_start;
 	char *request_type, *file_path, *protocol;
 	FILE *requested_file;
@@ -212,7 +218,7 @@ void handleread(struct con *cp, char *arg_path)
 		tmp = realloc(cp->buf, (cp->bs + BUF_ASIZE) * sizeof(char));
 		if (tmp == NULL) {
 			/* we're out of memory */
-			closecon(cp, 0);
+			closecon(cp, 0, argv);
 			return;
 		}
 		cp->buf = tmp;
@@ -235,14 +241,14 @@ void handleread(struct con *cp, char *arg_path)
 	i = read(cp->sd, cp->bp, cp->bl);
 	if (i == 0) {
 		/* 0 byte read means the connection got closed */
-		closecon(cp, 0);
+		closecon(cp, 0, argv);
 		return;
 	}
 	if (i == -1) {
 		if (errno != EAGAIN) {
 			/* read failed */
 			err(1, "read failed! sd %d\n", cp->sd);
-			closecon(cp, 0);
+			closecon(cp, 0, argv);
 		}
 		/*
 		 * note if EAGAIN, we just return, and let our caller
@@ -251,7 +257,7 @@ void handleread(struct con *cp, char *arg_path)
 		return;
 	}
 	/*
-	 * ok we really got something read. chage where we're
+	 * ok we really got something read. change where we're
 	 * pointing
 	 */
 	cp->bp += i;
@@ -263,49 +269,218 @@ void handleread(struct con *cp, char *arg_path)
 	 */
 	if (*(cp->bp - 1) == '\n') {
 		cp->state = STATE_WRITING;
-		cp->bl = cp->bp - cp->buf; /* how much will we write */
-		cp->bp = cp->buf;	   /* and we'll start from here */
-		fprintf(err_file, "Before calloc\n");
-		request_type = calloc(cp->bs, sizeof(char));
-		file_path = calloc(cp->bs + strlen(arg_path), sizeof(char));
-		protocol = calloc(cp->bs, sizeof(char));
-		fprintf(err_file, "Before scan\n");
-		sscanf(cp->bp, "%s %s %s", request_type, file_path, protocol);
-		fprintf(err_file, "%s %s %s", request_type,file_path, protocol);
+		cp->bl = cp->bp - cp->buf; 
+		cp->bp = cp->buf;
+		request_type = calloc(cp->bl, sizeof(char));
+		file_path = calloc(cp->bl + strlen(argv[2]), sizeof(char));
+		protocol = calloc(cp->bl, sizeof(char));
+		sscanf(cp->buf, "%s %s %s", request_type, file_path, protocol);
+                c1 = *(cp->buf + strlen(cp->buf) -2);
+                c2 = *(cp->buf + strlen(cp->buf) -1);
+                if(!( ((c1 == '\n') && (c2 == '\n')) || ((c1 =='\r') && (c2=='\n')) )){
+                   char fail_string[1000];
+                   char date_string[MAX_DATE_STRING];
+                   memset(date_string, '\0', MAX_DATE_STRING);
+                   memset(fail_string, '\0', 1000);
+                   get_date(date_string);
+                   strcpy(fail_string, "HTTP/1.1 400 Bad Request\nDate: ");
+                   strcat(fail_string, date_string);
+                   strcat(fail_string, "\nContent-Type: text/html\nContent-Length: 107\n\n<html><body>\n<h2>Malformed Request</h2>\nYour browser sent a request I could not understand.\n</body></html>");
+		  free(cp->buf);
+		  cp->bl = strlen(fail_string);
+		  cp->bp = fail_string;
+		  cp->buf = fail_string;
+                  strcpy(cp->log_string, date_string);
+                  strcat(cp->log_string, "\t");
+                  //strcat(cp->log_string, inet_ntoa((cp->sa).sin_addr));
+                  strcat(cp->log_string, "\t");
+                  strcat(cp->log_string, request_type);
+                  strcat(cp->log_string, " ");
+                  strcat(cp->log_string, file_path);
+                  strcat(cp->log_string, protocol);
+                  strcat(cp->log_string, "\t");
+                  strcat(cp->log_string, "400 Bad Request");
+                  cp->is_OK = 0;
+		  free(request_type);
+		  free(file_path);
+		  free(protocol);
+                  return;
+                  }
+                else{
+		complete_path = calloc((strlen(argv[1]) + strlen(file_path)+2), 
+				       sizeof(char));
+		strcpy(complete_path, ".");
+		strcat(complete_path, argv[2]);
+		strcat(complete_path, file_path);
+                char date_string[MAX_DATE_STRING];
+                memset(date_string, '\0', MAX_DATE_STRING);
+                get_date(date_string);
 		if(!strcmp(request_type,"GET") && !strcmp(protocol,"HTTP/1.1")){
-		  requested_file = fopen(strcat(arg_path, file_path), "r");
-		    fprintf(err_file, "before file load\n");
-		    fflush(err_file);
-		    file_start = str_from_file(requested_file, &file_size);
+		  if((requested_file = fopen(complete_path, "r")) == NULL){
+		   char fail_string[1000];
+                   memset(fail_string, '\0', 1000);
+
+                   if(errno == EACCES){
+                           strcpy(fail_string, "HTTP/1.1 403 Forbidden\nDate: ");
+                           strcat(fail_string, date_string);
+                           strcat(fail_string, "\nContent-Type: text/html\nContent-Length: 117\n\n<html><body>\n<h2>Permission Denied</h2>\nYou asked for a document you are not permitted to see. It sucks to be you.\n</body></html>");
+
+	        	  free(cp->buf);
+	        	  cp->bp = fail_string;
+	        	  cp->buf = fail_string;
+                          cp->bl = strlen(fail_string);
+                          strcpy(cp->log_string, date_string);
+                          strcat(cp->log_string, "\t");
+                          /*char* ip = inet_ntoa(((cp->sa).sin_addr));
+                          strcat(cp->log_string, ip);*/
+                          strcat(cp->log_string, "\t");
+                          strcat(cp->log_string, request_type);
+                          strcat(cp->log_string, " ");
+                          strcat(cp->log_string, file_path);
+                          strcat(cp->log_string, protocol);
+                          strcat(cp->log_string, "\t");
+                          strcat(cp->log_string, "403 Forbidden");
+                          cp->is_OK = 0;
+	        	  free(request_type);
+	        	  free(file_path);
+	        	  free(protocol);
+	        	  free(complete_path);
+                          return;
+                       }
+                  else{
+                           strcpy(fail_string, "HTTP/1.1 404 Not Found\nDate: ");
+                           strcat(fail_string, date_string);
+                           strcat(fail_string, "\nContent-Type: text/html\nContent-Length: 117\n\n<html><body>\n<h2>Document not found</h2>\nYou asked for a document that doesn\'t exist. That is so sad.\n</body></html>");
+	        	  free(cp->buf);
+	        	  cp->bp = fail_string;
+	        	  cp->buf = fail_string;
+                          cp->bl = strlen(fail_string);
+                          strcpy(cp->log_string, date_string);
+                          strcat(cp->log_string, "\t");
+                          //strcat(cp->log_string, inet_ntoa((cp->sa).sin_addr));
+                          strcat(cp->log_string, "\t");
+                          strcat(cp->log_string, request_type);
+                          strcat(cp->log_string, " ");
+                          strcat(cp->log_string, file_path);
+                          strcat(cp->log_string, protocol);
+                          strcat(cp->log_string, "\t");
+                          strcat(cp->log_string, "404 Not Found");
+                          cp->is_OK = 0;
+	        	  free(request_type);
+	        	  free(file_path);
+	        	  free(protocol);
+	        	  free(complete_path);
+                          return;
+		  }
+                }
+                else{
+		  if((req_filed = open(complete_path)) < 0)
+		    file_start = str_from_file(requested_file, req_filed, &file_size);
 		    fclose(requested_file);
 		    free(cp->buf);
-		    cp->bl = file_size;
 		    cp->bp = file_start;
 		    cp->buf = file_start;
-		    cp->bs = file_size;
+                    cp->bl = strlen(file_start);
+                    strcpy(cp->log_string, date_string);
+                    strcat(cp->log_string, "\t");
+                    //strcat(cp->log_string, inet_ntoa((cp->sa).sin_addr));
+                    strcat(cp->log_string, "\t");
+                    strcat(cp->log_string, request_type);
+                    strcat(cp->log_string, " ");
+                    strcat(cp->log_string, file_path);
+                    strcat(cp->log_string, protocol);
+                    strcat(cp->log_string, "\t");
+                    strcat(cp->log_string, "200 OK ");
+                    snprintf(itoa,12, "%d", file_size);
+                    //cp->file_size = itoa;
+                    cp->is_OK = 1;
+		    close(req_filed);
 		    free(request_type);
 		    free(file_path);
 		    free(protocol);
-		  }
-	}
-	fclose(err_file);
+		    free(complete_path);
+                    fprintf(err_file, "After struct setup: %s\n", cp->buf);
+                    fflush(err_file);
+                    return;
+                }
+        }
+		else{
+		   char fail_string[1000];
+                   char date_string[MAX_DATE_STRING];
+                   memset(date_string, '\0', MAX_DATE_STRING);
+                   memset(fail_string, '\0', 1000);
+                   get_date(date_string);
+                   strcpy(fail_string, "HTTP/1.1 400 Bad Request\nDate: ");
+                   strcat(fail_string, date_string);
+                   strcat(fail_string, "\nContent-Type: text/html\nContent-Length: 107\n\n<html><body>\n<h2>Malformed Request</h2>\nYour browser sent a request I could not understand.\n</body></html>");
+		  free(cp->buf);
+		  cp->bp = fail_string;
+		  cp->buf = fail_string;
+                  cp->bl = strlen(fail_string);
+                  strcpy(cp->log_string, date_string);
+                  strcat(cp->log_string, "\t");
+                  //strcat(cp->log_string, inet_ntoa((cp->sa).sin_addr));
+                  strcat(cp->log_string, "\t");
+                  strcat(cp->log_string, request_type);
+                  strcat(cp->log_string, " ");
+                  strcat(cp->log_string, file_path);
+                  strcat(cp->log_string, protocol);
+                  strcat(cp->log_string, "\t");
+                  strcat(cp->log_string, "400 Bad Request");
+                  cp->is_OK = 0;
+		  free(request_type);
+		  free(file_path);
+		  free(protocol);
+		  free(complete_path);
+                  return;
+		}
+            }      
+        }
 }
-
-char *str_from_file(FILE *requested_file, int *file_size){
+char *str_from_file(FILE *requested_file, int req_filed, int *file_size){
 	char c;
 	char *file_start, *file_position;
-        struct stat file_stat;
-	fstat(requested_file, &file_stat);
-	*file_size = file_stat.st_size;
-	file_start = malloc(*file_size + 200);
+	struct stat file_stat;
+	
+	//getting a time string following the example on the man page for 
+	//strftime
+	char date_time[MAX_DATE_STRING]; 
+        get_date(date_time);
+
+
+	fstat(req_filed, &file_stat);
+	*file_size = ((int)((u_long)file_stat.st_size));
+	
+	file_start = malloc(*file_size + 300);
 	file_position = file_start;
-	strcpy(file_start, "HTTP/1.1 200 OK\nDate: Mon 21 Jan 2008 18:06:16 GMT\nContent-Type: text/html\nContent-Length: 1234");
+	strcpy(file_start, "HTTP/1.1 200 OK\nDate: ");
+	strcat(file_start, date_time);
+        sprintf(file_start, "%s\nContent-Type: text/html\nContent-Length: %d\n", file_start, *file_size);
+	*file_size += strlen(file_start);
+	fprintf(err_file, "file size: %d\n", *file_size);
 	file_position = file_start + strlen(file_start);
+		    fprintf(err_file, "before loop\n");
+		    fflush(err_file);
 	while((c = fgetc(requested_file)) != EOF){
 	  *file_position = c;
 	  file_position++;
 	}
 	return file_start;
+}
+
+void get_date(char *date_time){
+	time_t t;
+	struct tm * tmp;
+	t = time(NULL);
+	tmp = localtime(&t);
+	if(tmp == NULL){
+	  perror("localtime");
+	  exit(EXIT_FAILURE);
+	}
+	if(strftime(date_time, MAX_DATE_STRING, "%a %d %b %G %H:%M:%S %Z",tmp) == 0){
+	  perror("strftime returned 0");
+	  exit(EXIT_FAILURE);
+	}
 }
 
 int main(int argc,  char *argv[])
@@ -384,10 +559,10 @@ int main(int argc,  char *argv[])
 	 */
 	printf("Server up and listening for connections on port %u\n", port);
 #endif	
-
+        
 	/* initialize all our connection structures */
 	for (i = 0; i < MAXCONN; i++)
-		closecon(&connections[i], 1);
+		closecon(&connections[i], 1, argv);
 
 	for(;;) {
 		int i;
@@ -409,6 +584,7 @@ int main(int argc,  char *argv[])
 			if (connections[i].sd > max)
 				max = connections[i].sd;
 		}
+
 		if (max > omax) {
 			/* we need bigger fd_sets allocated */
 
@@ -540,10 +716,10 @@ int main(int argc,  char *argv[])
 			for (i = 0; i<MAXCONN; i++) {
 				if ((connections[i].state == STATE_READING) &&
 				    FD_ISSET(connections[i].sd, readable))
-				  handleread(&connections[i], argv[2]);
+				  handleread(&connections[i], argv);
 				if ((connections[i].state == STATE_WRITING) &&
 				    FD_ISSET(connections[i].sd, writable))
-					handlewrite(&connections[i]);
+					handlewrite(&connections[i], argv);
 			}
 		}
 	}
